0eee487cd588a75d814020ddcbd67bf9025a3b42
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-06-26 11:36:20 +0100

    bau: log errors
    Pair: WillPa

--- src/main/java/uk/gov/ida/stub/idp/exceptions/mappers/SessionSerializationExceptionMapper.java
+++ src/main/java/uk/gov/ida/stub/idp/exceptions/mappers/SessionSerializationExceptionMapper.java
@@ -1,5 +1,6 @@
 package uk.gov.ida.stub.idp.exceptions.mappers;
 
+import org.apache.log4j.Logger;
 import uk.gov.ida.stub.idp.exceptions.SessionSerializationException;
 import uk.gov.ida.stub.idp.views.ErrorPageView;
 
@@ -7,8 +8,10 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 public class SessionSerializationExceptionMapper implements ExceptionMapper<SessionSerializationException> {
+	private static final Logger LOG = Logger.getLogger(SessionSerializationExceptionMapper.class);
 	@Override
 	public Response toResponse(SessionSerializationException exception) {
+		LOG.error(exception);
 		return Response
 				.status(Response.Status.INTERNAL_SERVER_ERROR)
 				.entity(new ErrorPageView())
