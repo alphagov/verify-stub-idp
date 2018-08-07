549f518d7f6107bfe568bdc0c50a88c9a83d0166
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2018-08-07 16:00:34 +0100

    WIP

---
+++ jenkins.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+stub_idp_fedconfig="$(pwd)/../verify-stub-idp-federation-config"
+
+# Copy stub-idps.yml for each env into zip
+mkdir -p stub-idp/src/dist/resources
+rsync -qrv "$stub_idp_fedconfig/configuration/" stub-idp/src/dist/resources/
+
+./gradlew --parallel --no-daemon \
+  clean test intTest
+
+./gradlew --no-daemon \
+  -Pversion=$BUILD_NUMBER \
+  -PstubidpExtraLogosDirectory="$stub_idp_fedconfig/idp-logos" \
+  copyToLib distZip publish bintrayUpload
+
+bin/build.rb
+
+./gradlew --no-daemon outputDependencies -q > dependencies.properties
