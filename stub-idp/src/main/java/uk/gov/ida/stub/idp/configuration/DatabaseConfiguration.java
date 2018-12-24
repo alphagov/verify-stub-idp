924582c84331a1893d32d6121ca74933b11ad3b8
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-12-24 11:00:43 +0000

    Allow DatabaseConfiguration to use VCAP_SERVICES
    We should be able to look at VCAP_SERVICES provided to us by Cloud
    Foundry which provides us a `jdbcuri` field
    
    The JSON path is `/postgres/0/credentials/jdbcuri`
    
    solo @tlwr

--- stub-idp/src/main/java/uk/gov/ida/stub/idp/configuration/DatabaseConfiguration.java
+++ stub-idp/src/main/java/uk/gov/ida/stub/idp/configuration/DatabaseConfiguration.java
@@ -2,17 +2,49 @@ package uk.gov.ida.stub.idp.configuration;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import javax.validation.constraints.NotNull;
+import java.io.IOException;
+import java.util.Optional;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DatabaseConfiguration {
 
-    @NotNull
+
     @JsonProperty("url")
     private String url;
 
+
+    @JsonProperty("vcapServices")
+    private String vcapServices;
+
+    private String getUrlFromVcap(String vcapServices) {
+        return Optional
+                .ofNullable(vcapServices)
+                .map(val -> {
+                    try {
+                        ObjectMapper mapper = new ObjectMapper();
+                        return mapper.readTree(val);
+                    }
+                    catch (IOException e) {
+                        throw new RuntimeException("IOException when parsing VCAP_SERVICES environment variable");
+                    }
+                })
+                .map(vcap -> vcap.get("postgres"))
+                .map(postgresDatabases -> postgresDatabases.get(0))
+                .map(postgresDatabase -> postgresDatabase.get("credentials"))
+                .map(credentials -> credentials.get("jdbcuri"))
+                .map(jdbcuri -> jdbcuri.textValue())
+                .orElseThrow(() -> new RuntimeException("Could not parse vcap services"));
+    }
+
     public String getUrl() {
-        return url;
+        if (vcapServices != null && vcapServices.length() > 0) {
+            return getUrlFromVcap(vcapServices);
+        } else if (url != null && url.length() > 0) {
+            return url;
+        }
+
+        throw new RuntimeException("Neither url nor vcapServices was workable in database configuration");
     }
 }
---
+++ stub-idp/src/test/java/uk/gov/ida/stub/idp/configuration/DatabaseConfigurationTest.java
@@ -0,0 +1,90 @@
+package uk.gov.ida.stub.idp.configuration;
+
+import io.dropwizard.configuration.YamlConfigurationFactory;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static io.dropwizard.jackson.Jackson.newObjectMapper;
+import static io.dropwizard.jersey.validation.Validators.newValidator;
+import static org.hamcrest.core.StringContains.containsString;
+
+public class DatabaseConfigurationTest extends BaseConfigurationTest {
+
+    @Rule
+    public final ExpectedException expectedException = ExpectedException.none();
+    private final YamlConfigurationFactory factory = new YamlConfigurationFactory<>(
+            DatabaseConfiguration.class, newValidator(), newObjectMapper(), "dw.");
+
+    @Test
+    public void shouldThrowRTEIfNoUrlorVcap() throws Exception {
+        expectedException.expectMessage(containsString("Neither url nor vcapServices was workable"));
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider("aField: {}"),
+                ""
+        );
+
+        dbCfg.getUrl();
+    }
+
+    @Test
+    public void shouldReturnUrlIfOnlyUrlProvided() throws Exception {
+        String url = "jdbc:postgresql://db.example.com:5432/broker?password=dbpassword&ssl=true&user=dbuser";
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider(
+                        "url: " + url
+                ),
+                ""
+        );
+
+        assert dbCfg.getUrl().equals(url);
+    }
+
+    @Test
+    public void shouldReturnVcapIfOnlyVcapProvided() throws Exception {
+        String url = "jdbc:postgresql://db.example.com:5432/broker?password=dbpassword&ssl=true&user=dbuser";
+        String vcap = "{\"postgres\":[{ \"credentials\": { \"jdbcuri\": \"" + url + "\"}}]}";
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider("vcapServices: '" + vcap + "'"), ""
+        );
+
+        assert dbCfg.getUrl().equals(url);
+    }
+
+    @Test
+    public void shouldReturnVcapIfBothUrlAndVcapProvided() throws Exception {
+        String url = "jdbc:postgresql://db.example.com:5432/broker?password=dbpassword&ssl=true&user=dbuser";
+        String vcap = "{\"postgres\":[{ \"credentials\": { \"jdbcuri\": \"" + url + "\"}}]}";
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider("url: idontcare\nvcapServices: '" + vcap + "'"), ""
+        );
+
+        assert dbCfg.getUrl().equals(url);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfUrlIsEmpty() throws Exception {
+        expectedException.expectMessage(containsString("Neither url nor vcapServices was workable"));
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider("url: ''"), ""
+        );
+
+        dbCfg.getUrl();
+    }
+
+    @Test
+    public void shouldThrowExceptionIfVcapIsEmpty() throws Exception {
+        expectedException.expectMessage(containsString("Neither url nor vcapServices was workable"));
+
+        DatabaseConfiguration dbCfg = (DatabaseConfiguration) factory.build(
+                new StringConfigurationSourceProvider("vcap: ''"), ""
+        );
+
+        dbCfg.getUrl();
+    }
+}
