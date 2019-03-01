546c6c18cc95123cd419f67e9f7a5f3c32ef56f0
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-03-01 20:18:55 +0000

    Tidy EidasProfileValidationSpecification
    This class can be package-private and there's no need to mark the static
    methods final

--- stub-idp/src/integration-test/java/uk/gov/ida/apprule/support/eidas/EidasProfileValidationSpecification.java
+++ stub-idp/src/integration-test/java/uk/gov/ida/apprule/support/eidas/EidasProfileValidationSpecification.java
@@ -5,16 +5,16 @@ import uk.gov.ida.saml.core.validation.SamlTransformationErrorException;
 
 import java.text.MessageFormat;
 
-public class EidasProfileValidationSpecification {
+class EidasProfileValidationSpecification {
 
-    public static final SamlTransformationErrorException authnResponseAssertionIssuerFormatMismatch(String responseIssuerFormat, String assertionIssuerFormat) {
+    static SamlTransformationErrorException authnResponseAssertionIssuerFormatMismatch(String responseIssuerFormat, String assertionIssuerFormat) {
         return new SamlTransformationErrorException(
                 MessageFormat.format("The Authn Response issuer format [{0}] does not match the assertion issuer format [{1}]", responseIssuerFormat, assertionIssuerFormat),
                 Level.ERROR
         );
     }
 
-    public static final SamlTransformationErrorException authnResponseAssertionIssuerValueMismatch(String responseIssuer, String assertionIssuer) {
+    static SamlTransformationErrorException authnResponseAssertionIssuerValueMismatch(String responseIssuer, String assertionIssuer) {
         return new SamlTransformationErrorException(
                 MessageFormat.format("The Authn Response issuer [{0}] does not match the assertion issuer [{1}]", responseIssuer, assertionIssuer),
                 Level.ERROR
