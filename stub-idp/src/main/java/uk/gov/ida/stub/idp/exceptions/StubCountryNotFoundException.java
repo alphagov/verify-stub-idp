63bb98616bb559a833b4d2a9c7a3639b554176c3
Russell Howe <russell.howe@digital.cabinet-office.gov.uk>
2019-03-04 09:59:15 +0000

    Remove unused classes
    - PrivateKeyStoreFactory
    - SamlTransformationErrorManagerTestHelper
    - StringEncoding
    - StubCountryNotFoundException

--- stub-idp/src/main/java/uk/gov/ida/stub/idp/exceptions/StubCountryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.ida.stub.idp.exceptions;
-
-public class StubCountryNotFoundException extends RuntimeException {
-    public StubCountryNotFoundException(String message) {
-        super(message);
-    }
-}
