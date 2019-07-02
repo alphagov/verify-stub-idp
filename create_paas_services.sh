ffcb610c877dd02254e14c6c2ade7797df3d6906
Aditya Pahuja <aditya.pahuja@digital.cabinet-office.gov.uk>
2019-07-02 11:46:33 +0100

    AUTO-112: Add a new manifest template file for PaaS
    Adds a new manifest template file. This file will replace manifest.yml and will be used for deploy stub-idp application to all environments in PaaS.
    Update create_paas_services.sh to use updated service names e.g. stub-idp-db-$ENV and stub-idp-logit-$ENV.
    Add build.yml file for building verify-stub-idp zip file using OpenJDK 11. This file can be deployed to all environments in PaaS.
    
    Author: @adityapahuja

--- create_paas_services.sh
+++ create_paas_services.sh
@@ -4,8 +4,8 @@ set -eu
 
 cf target -s "$CF_SPACE"
 
-echo Creating ida-stub-idp-db Postgres service
-cf create-service postgres Free ida-stub-idp-db
+echo Creating stub-idp-db-$ENV Postgres service
+cf create-service postgres Free stub-idp-db-$ENV
 
-echo Creating ida-stub-idp-logit user-defined service
-cf create-user-provided-service ida-stub-idp-logit -l "syslog-tls://${LOGIT_ENDPOINT}:21541"
+echo Creating stub-idp-logit-$ENV user-defined service
+cf create-user-provided-service stub-idp-logit-$ENV -l "syslog-tls://${LOGIT_ENDPOINT}:21541"
